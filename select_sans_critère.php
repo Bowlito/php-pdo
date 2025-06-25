0ad83439e0fe7a98867495d7040b3532411558c2
Bowlito <julien.catoio@gmail.com>
2025-06-25 14:25:37 +0200

    resulat de la requête

--- select_sans_critère.php
+++ select_sans_critère.php
@@ -14,7 +14,17 @@ try {
     // Pour exécuter le SQL
     $query = $pdo->query($select);
 
-    //Récupérer le résultat
+    //Récupérer le 1er tuple
+    $resultat = $query->fetch();
+    
+    echo "\n$resultat[0] $resultat[1] $resultat[2] $resultat[3]";
+
+    //Cibler avec le nom du champs
+
+    echo "\n" . $resultat['id']. "";
+    
+
+    //Récupérer tous les tuples de la table
 
     $resultat = $query->fetchAll();
     foreach ($resultat as $key => $value) {
