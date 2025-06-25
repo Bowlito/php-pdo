8f5e13576aa1cddee13b3228553b0a0e9a7fbc36
Bowlito <julien.catoio@gmail.com>
2025-06-25 14:15:58 +0200

    connexion avec la BDD et traitement des exceptions

--- select_sans_critère.php
+++ select_sans_critère.php
@@ -1,11 +1,25 @@
 <?php
 
-$mysql_username="root";
-$mysql_password="";
-$mysql_db="php_pdo";
+$mysql_username = "root";
+$mysql_password = "";
+$mysql_db = "php_pdo";
 
 // DSN : Data Source Name : URL de connexion
+try {
+    $dsn = "mysql:host=localhost;port=3306;dbname=$mysql_db;charset=utf8";
+    // Etablir la connexion avec MySQL
+    $pdo = new PDO($dsn, $mysql_username, $mysql_password);
+    // chaîne contenant la requête SQL
+    $select = "SELECT * FROM utilisateurs";
+    // Pour exécuter le SQL
+    $query = $pdo->query($select);
 
-$dsn = "mysql:host=localhost;port=3306;dbname=$mysql_db;charset=utf8";
+    //Récupérer le résultat
 
-$pdo = new PDO($dsn,$mysql_username,$mysql_password);
+    $resultat = $query->fetchAll();
+    foreach ($resultat as $key => $value) {
+        echo "\n $value[0] $value[1] $value[2] $value[3]";
+    }
+} catch (PDOException $ex) {
+    echo "\nErreur : problème de connexion avec la BDD\n" . $ex->getMessage();
+}
