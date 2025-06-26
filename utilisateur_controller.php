a5e1adfb05ed23522e431ba818b3fc5614735723
Bowlito <julien.catoio@gmail.com>
2025-06-26 11:37:05 +0200

    Goat solution marqueur nominatif

--- utilisateur_controller.php
+++ utilisateur_controller.php
@@ -1,5 +1,5 @@
 <?php
-// solution avec les fonctions de transformation : htmlentities
+// solution avec un marqueur nominatif
 $name = $_POST['username'];
 $pwd = $_POST['password'];
 
@@ -10,19 +10,45 @@ $mysql_db = "php_pdo";
 try {
     $dsn = "mysql:host=localhost;port=3306;dbname=$mysql_db;charset=utf8";
     $pdo = new PDO($dsn, $mysql_username, $mysql_password);
-    // Injection SQL avec gfghfgf' or true !='sqdsq
-    $select = "SELECT * FROM utilisateurs WHERE username = '" .  htmlentities($name) . "'  AND password = '" . htmlentities($pwd) ."'";
-    $query = $pdo->query($select);
+    $select = "SELECT * FROM utilisateurs WHERE username = :name AND password = :pwd";
+    echo "<br>$select<br>";
+    $query = $pdo->prepare($select);
+    $query->bindValue(":name", $name);
+    $query->bindValue(":pwd", $pwd);
+    $query->execute();
     $resultat = $query->fetch();
     if ($resultat != false) {
         echo "Vous êtes bien connectés : $name -- $pwd";
     } else {
         echo "Identifiants incorrects";
     }
-
 } catch (PDOException $ex) {
     echo "\nErreur : problème de connexion avec la BD" . $ex->getMessage();
 }
+// // solution avec les fonctions de transformation : htmlentities
+// $name = $_POST['username'];
+// $pwd = $_POST['password'];
+
+// $mysql_username = "root";
+// $mysql_password = "";
+// $mysql_db = "php_pdo";
+
+// try {
+//     $dsn = "mysql:host=localhost;port=3306;dbname=$mysql_db;charset=utf8";
+//     $pdo = new PDO($dsn, $mysql_username, $mysql_password);
+//     // Injection SQL avec gfghfgf' or true !='sqdsq
+//     $select = "SELECT * FROM utilisateurs WHERE username = '" .  htmlentities($name) . "'  AND password = '" . htmlentities($pwd) ."'";
+//     $query = $pdo->query($select);
+//     $resultat = $query->fetch();
+//     if ($resultat != false) {
+//         echo "Vous êtes bien connectés : $name -- $pwd";
+//     } else {
+//         echo "Identifiants incorrects";
+//     }
+
+// } catch (PDOException $ex) {
+//     echo "\nErreur : problème de connexion avec la BD" . $ex->getMessage();
+// }
 // Solution avec le marqueur ?
 // $name = $_POST['username'];
 // $pwd = $_POST['password'];
